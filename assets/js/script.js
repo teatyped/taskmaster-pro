e9e18efaa0170833dfec685d35110e151fef70d6
Khai Truong <kminht@gmail.com>
2022-03-17 23:30:01 -0400

    on click of tasklistitem to be able to edit and save edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,48 @@ var loadTasks = function() {
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
+// on click to <p> in to do list
+$(".list-group").on("click", "p", function () {
+  var text = $(this)
+  .text()
+  .trim();
+  var textInput = $("<textarea>")
+  .addClass("from-control")
+  .val(text);
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+
+  console.log(text);
+});
+
+$(".list-group").on("blur", "textarea", function () {
+  var text = $(this)
+    .val() 
+    .trim();
+
+  //get parents ul id 
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+    //get the task postion in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+    tasks[status][index].text = text;
+    saveTasks();
+// recreate p element
+    var taskP =$("<p>")
+    .addClass("m-1")
+    .text(text);
+// replace textarea with p element 
+  $(this).replaceWith(taskP);
+
+})
+
+
 
 
 
