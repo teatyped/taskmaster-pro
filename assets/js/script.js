48255db194f9a9c7e6ea9778989b09aa3721f3d0
Khai Truong <kminht@gmail.com>
2022-03-18 21:23:05 -0400

    sortable list with updating info to tasks object.

--- assets/js/script.js
+++ assets/js/script.js
@@ -139,7 +139,58 @@ $(".list-group").on("blur", "input[type='text']", function() {
 });
 
 
+// sortable 
 
+$(".card .list-group"). sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false, 
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event){
+    console.log("active", this);
+  },
+  deactivate: function(event){
+    console.log("deactivate", this);
+  },
+  over: function(event){
+    console.log("over", event.target);
+  },
+  out: function(event){
+    console.log("out", event.target);
+  },
+  // update info into an array
+  update: function(event){
+    var tempArr =[];
+    //loop over current set of children in sort list
+    $(this).children().each(function(){
+      var text =$(this)
+        .find("p")
+        .text()
+        .trim();
+
+        var date = $(this)
+          .find("span")
+          .text()
+          .trim();
+          
+      console.log("pulled info " + text, date); //delete later used to track info
+      // add task data to the temp array as object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    //trim down list ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+    console.log(tempArr);
+    //update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+    console.log(tasks);
+  }
+});
 
 
 
